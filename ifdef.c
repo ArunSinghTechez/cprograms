34f820ec0abf8a190331098809324d5602ad9b37
ArunSinghTechez <mail4officials@gmail.com>
2024-02-01 21:50:27 +0530

    preprocessor ifdef macro

---
+++ ifdef.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#define check 
+
+
+
+
+
+int main(){
+    #ifdef check 
+    printf("\n program checked\n");
+    #else
+    printf("\n not defined\n");
+    #endif
+
+
+
+
+
+    getchar();
+    return 0;
+}
